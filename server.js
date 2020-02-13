2654e3cd501d7b28743f4b66449328fd9be9003d
Matthew Knight <mattknight.sa@gmail.com>
2020-02-13 10:28:16 -0600

    created files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const User = require("./userModel.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
